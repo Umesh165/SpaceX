f4014331caf4d755f4e218b5ccdf5e748613cb20
Umesh Sharma <umesh.sharma@hashedin.com>
2020-08-29 22:44:16 +0530

    All Filters working

--- src/app/Services/rockets.service.ts
+++ src/app/Services/rockets.service.ts
@@ -6,6 +6,10 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class RocketsService {
+  launch_year: string = '';
+  launch_success: string = '';
+  land_success = '';
+  filterString: string = '';
   constructor(private http: HttpClient) {}
 
   url = 'https://api.spaceXdata.com/v3/launches?limit=100';
@@ -14,7 +18,28 @@ export class RocketsService {
     return this.http.get(this.url);
   }
 
-  getRocketLaunchFilteredData(filter: string): Observable<any> {
-    return this.http.get(this.url + filter);
+  getRocketLaunchFilteredData(data: {
+    filter: string;
+    value: string;
+  }): Observable<any> {
+    console.log(this.filterString);
+    if (data.filter === '&launch_year') {
+      this.launch_year = data.filter + '=' + data.value;
+      this.filterString =
+        this.launch_success + this.land_success + this.launch_year;
+    }
+    if (data.filter === '&launch_success') {
+      this.launch_success = data.filter + '=' + data.value;
+      this.filterString =
+        this.launch_success + this.land_success + this.launch_year;
+    }
+    if (data.filter === '&land_success') {
+      this.land_success = data.filter + '=' + data.value;
+      this.filterString =
+        this.launch_success + this.land_success + this.launch_year;
+    }
+    console.log(this.filterString);
+    console.log(this.url + this.filterString);
+    return this.http.get(this.url + this.filterString);
   }
 }
--- src/app/spacex/home/home.component.ts
+++ src/app/spacex/home/home.component.ts
@@ -9,36 +9,15 @@ import { Observable } from 'rxjs';
 })
 export class HomeComponent implements OnInit {
   rocketLaunchData$: Observable<any>;
-  launch_year: string = '';
-  launch_success: string = '';
-  land_success = '';
-  filterString: string = '';
   constructor(private rocketsService: RocketsService) {}
 
   ngOnInit(): void {
     this.rocketLaunchData$ = this.rocketsService.getRocketLaunchData();
   }
 
-  getFilteredData = (data): void => {
-    console.log(data);
-    if (data.filter === 'launch_year') {
-      this.launch_year = data.filter + '=' + data.value;
-      this.filterString =
-        this.launch_success + '&' + this.land_success + '&' + this.launch_year;
-    }
-    if (data.filter === 'launch_success') {
-      this.launch_success = data.filter + '=' + data.value;
-      this.filterString =
-        this.launch_success + '&' + this.land_success + '&' + this.launch_year;
-    }
-    if (data.filter === 'land_success') {
-      this.land_success = data.filter + '=' + data.value;
-      this.filterString =
-        this.launch_success + '&' + this.land_success + '&' + this.launch_year;
-    }
-    console.log(this.filterString);
+  getFilteredData = (data: { filter: string; value: string }): void => {
     this.rocketLaunchData$ = this.rocketsService.getRocketLaunchFilteredData(
-      this.filterString
+      data
     );
   };
 }
